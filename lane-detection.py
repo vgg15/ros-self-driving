664a839e05df0165448a4d4ef9d822e207e96632
Francesco Viggiano <f.viggiano@ema-ic.it>
2019-04-20 20:20:52 +0200

    add progress bar

--- lane-detection.py
+++ lane-detection.py
@@ -1,18 +1,24 @@
 import cv2
 import numpy as np
 import math
+import time
+import sys
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MaxAbsScaler 
 from sklearn.model_selection  import train_test_split
 from sklearn.preprocessing import minmax_scale
 
+"""
 import keras
 from keras.layers import Dense
 from keras.models import Sequential
-
-import pandas as pd
+"""
 import os
+from os import system, name 
 from threading import Thread
+from queue import Queue
+import collections
 
 DATA_X_FILENAME = 'data_x'
 DATA_Y_FILENAME = 'data_y'
@@ -20,145 +26,187 @@ DATA_Y_FILENAME = 'data_y'
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
 
+NUM_THREADS = 2
 
 DATASET_FOLDER = 'driving_dataset/driving_dataset/'
 DATASET_FILENAME = DATASET_FOLDER  + 'data.txt'
 MODEL_FILENAME = 'model.h5'
 
 ANGLE_RESOLUTION = 0.1
+ANGLE_STEPS = int(2/ANGLE_RESOLUTION)+1
+
+def print_progress(progress):
+    totpercent = 0.0
+    for id, percent in progress.items():
+        totpercent = totpercent + percent
+
+    totpercent = totpercent/NUM_THREADS
+    bar = ('=' * int(totpercent/5)).ljust(20)        
+    sys.stdout.write("\rTotal progress [%s] %s%%" % (bar, totpercent))    
+    sys.stdout.flush()
 
 class ImportThread (Thread):
-   def __init__(self, id, linelist):
-      Thread.__init__(self)
-      self.linelist = linelist
-      self.id = id
-        
-   def run(self):
-        x = np.zeros((1,CAMERA_WIDTH*CAMERA_HEIGHT))
-        y = np.zeros(1)
+    def __init__(self, id, linelist, status):
+        Thread.__init__(self)
+        self.linelist = linelist
+        self.id = id
+        self.status = status
+        self.xlist = []
+        self.ylist = []
+
+    def run(self):        
         i=0
-        listlen = len(self.linelist)         
-        for line in self.linelist:
-            # open image in b&w
+        listlen = len(self.linelist)          
+        print("Thread " + str(self.id) + " started with " + str(listlen) + " elements")                        
+
+        for line in self.linelist:            
             row = line.split()
             img = str(row[0])
             angle = float(row[1])    
-            frame = cv2.imread(DATASET_FOLDER+img, cv2.COLOR_BGR2GRAY)
             
-            # transform frame to b&w
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # open image in b&w
+            frame = cv2.imread(DATASET_FOLDER+img, cv2.IMREAD_GRAYSCALE)
 
             # resize frame
             frame = cv2.resize(frame,(CAMERA_WIDTH,CAMERA_HEIGHT))
-
+            
             # unroll
             frame = np.reshape(frame, (1,CAMERA_WIDTH*CAMERA_HEIGHT))
-
-            x = np.append(x,frame,axis=0)
-            y = np.append(y,[angle])
             
-            i =i+1    
-            if self.id == 0:
-                print("Progress: {0:.2f} %".format(float(i)/listlen*100), end='\r')
-            #if i==20:
-            #    break
-        np.save(DATA_X_FILENAME+str(self.id), x)
-        np.save(DATA_Y_FILENAME+str(self.id), y)
+            self.xlist.append(frame[0,:])
+            self.ylist.append(angle)
 
-#video = cv2.VideoCapture("road_car_view.mp4")
-
-ANGLE_STEPS = int(2/ANGLE_RESOLUTION)+1
-
-
-
-# Import data
-print("> Importing data from " + DATASET_FILENAME)
-#f = open(DATASET_FILENAME)
-i = 0
-
-if os.path.isfile(DATA_X_FILENAME+'.npy') and os.path.isfile(DATA_Y_FILENAME+'.npy'):    
-    """with open(DATA_X_FILENAME) as f:
-        xlist = f.readlines()
-        x = np.array(xlist)
-    with open(DATA_Y_FILENAME) as f:
-        ylist = f.readlines()
-        y = np.array(ylist)"""
-    print("> Found data files..")
-    x= np.load(DATA_X_FILENAME+'.npy')
-    y=np.load(DATA_Y_FILENAME+'.npy')
-else:         
-    print("> Generating data files..")
-
-    with open(DATASET_FILENAME) as f:
-        lineList = f.readlines()
-
-    listlen = len(lineList)
+            i = i + 1    
+            progress = int(float(i)/listlen*100)
+            self.status.put([self.id, progress])
+            time.sleep(0.01)
     
-    print("Dataset has " + str(listlen)+ " samples")
-    threadnum = 12
-    chunks = np.array_split(np.array(lineList),threadnum)
-    threads = []
-    for i in range(threadnum):
-        threads.append(ImportThread(i, chunks[i].tolist()))
+    def save(self):       
+        print("Thread " + str(self.id) + " saving data...")               
+        np.save(DATA_X_FILENAME+"_"+str(self.id), np.around(np.array(self.xlist), decimals=4))
+        np.save(DATA_Y_FILENAME+"_"+str(self.id), np.around(np.array(self.ylist), decimals=4))
+
+def KerasGenerator(x,y, bs):
+    i=0
+    totlen = len(x)
+    while True:
+        img = []
+        labels = []
+        while len(img) < bs:
+            img.append(x[i,:]/255.0)
+            labels.append(y[i,:])
+            i = i + 1
+            if i >= totlen:
+                i=0
+        yield (img, labels)
+
+def main():
+    # Import data
+    print("> Importing data from " + DATASET_FILENAME)
     
-    for t in threads:
-        t.start()
+    if os.path.isfile(DATA_X_FILENAME+'.npy') and os.path.isfile(DATA_Y_FILENAME+'.npy'):    
+        print("> Importing existing data files...")
+        x = np.load(DATA_X_FILENAME+'.npy')
+        y = np.load(DATA_Y_FILENAME+'.npy') 
+    else:         
+        print("> Generating new data files..")
+        millis = int(round(time.time() * 1000))
+        with open(DATASET_FILENAME) as f:
+            lineList = f.readlines()
+
+        listlen = len(lineList)
+        
+        print("Dataset has " + str(listlen)+ " samples")
+        status = Queue()
+        progress = collections.OrderedDict()
+        
+        chunks = np.array_split(np.array(lineList), NUM_THREADS)
+        threads = []
+        for i in range(NUM_THREADS):
+            t = ImportThread(i, chunks[i].tolist(), status)
+            threads.append(t)
+            progress[i] = 0.0
+            t.daemon = True
+            t.start()
+
+        while any(i.is_alive() for i in threads):
+            time.sleep(1)
+            while not status.empty():
+                id, percent = status.get()
+                progress[id] = percent
+                print_progress(progress)
+        
+        print("")                
+        for t in threads:
+            t.save()
+
+        for t in threads:
+            t.join()
+
+        millis2 = int(round(time.time() * 1000))                
+        print("> Generation done in %s seconds" % str((millis2-millis)/1000))
+
+        exit()
+    print("x shape:" + str(np.shape(x)))
+    print("x samples: ")
+    print(np.random.permutation(x)[1:5,:])
+
+    print("y shape:" + str(np.shape(y)))
+    print("y samples: ")
+    print(np.random.permutation(y)[1:5])
     
+    # Prepare data
+    print("> Preparing data")
+    #x = StandardScaler().fit_transform(x)
+    y_norm = MaxAbsScaler().fit_transform((StandardScaler().fit_transform(y.reshape(-1, 1))))
+    y = np.zeros((y_norm.shape[0], ANGLE_STEPS))    
+    for i,angle in enumerate(y_norm):
+        idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
+        y[i,math.ceil(idx)] = 1
 
-    for t in threads:
-        t.join()
+    print("x shape:" + str(np.shape(x)))
+    print("x samples: ")
+    print(np.random.permutation(x)[1:5,:])
 
+    print("y shape:" + str(np.shape(y)))
+    print("y samples: ")
+    print(np.random.permutation(y)[1:5]) 
 
-    """f = open(DATA_X_FILENAME, "w")
-    for i in range(x.shape[0]):
-        f.write(str(np.array2string(x[i,:], separator=' ', threshold = CAMERA_WIDTH*CAMERA_HEIGHT*10))+'\n')
-    f.close
-    f = open(DATA_Y_FILENAME, "w")
-    for i in range(y.shape[0]):
-        f.write(str(y[i]) + '\n')
-    f.close"""
+    #print("> Saving normalized data...")
+    #np.save(DATA_X_FILENAME+"_norm", np.around(x, decimals=4))
+    #np.save(DATA_Y_FILENAME+"_norm", np.around(y, decimals=4))
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .2)
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = .2)
+
+    # build simple NN
+    print("> Build Network")
+    input_units = x_train.shape[1]
+    inner_units = int(input_units/10)
+    output_units = ANGLE_STEPS # angle ranges from -1 to 1 in steps of 0.1
+
+    """
+    model = Sequential()
+    model.add(Dense(units=inner_units, activation='relu', input_dim=input_units))
+    model.add(Dense(units=inner_units, activation='relu'))
+    model.add(Dense(units=output_units, activation='softmax'))
+
+    print("> Compile and fit the Network")
+    BATCH_SIZE = 64
+    kg_train = KerasGenerator(x_train, y_train, bs)
+    kg_val = KerasGenerator(x_val,y_val, bs)
     
+    model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+    model.fit_generator( kg_train, steps_per_epoch=math.ceil(len(x_train) / BATCH_SIZE),
+        validation_data=kg_val,
+	    validation_steps=math.ceil(len(x_train) / BATCH_SIZE),
+	    epochs=5)        
 
-# Prepare data
-print("> Prepare data")
-x = StandardScaler().fit_transform(x)
-y_norm = MaxAbsScaler().fit_transform((StandardScaler().fit_transform(y.reshape(-1, 1))))
-y = np.zeros((y_norm.shape[0], ANGLE_STEPS))    
-for i,angle in enumerate(y_norm):
-    idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
-    y[i,math.ceil(idx)] = 1
+    model.save(MODEL_FILENAME)
 
-print("x shape:" + str(np.shape(x)))
-print("x sample: ")
-print(np.random.permutation(x)[1:5,:])
+    loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+    print(loss_and_metrics)
+    """
 
-print("y shape:" + str(np.shape(y)))
-print(np.random.permutation(y)[1:5,:])
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .1)
-
-# build simple NN
-print("> Build Network")
-input_units = x_train.shape[1]
-inner_units = int(input_units/10)
-output_units = ANGLE_STEPS # angle ranges from -1 to 1 in steps of 0.1
-
-model = Sequential()
-model.add(Dense(units=inner_units, activation='relu', input_dim=input_units))
-model.add(Dense(units=inner_units, activation='relu'))
-model.add(Dense(units=output_units, activation='softmax'))
-
-print("> Compile and fit the Network")
-model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs = 10, validation_split = .1)
-model.save(MODEL_FILENAME)
-
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-print(loss_and_metrics)
-#predict(x, batch_size=None, verbose=0, steps=None, callbacks=None)
-
-#key = cv2.waitKey(25)
-#if key == 27:
-#    break
-#video.release()
-#cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
