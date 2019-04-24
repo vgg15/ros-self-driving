6d63c7c134941598d1b30727e8b001a262ef5bf1
Francesco Viggiano <f.viggiano@ema-ic.it>
2019-04-24 18:56:54 +0200

    improving model

--- lane-detection.py
+++ lane-detection.py
@@ -1,3 +1,4 @@
+#!/home/alessandro/venv/bin/python3
 import cv2
 import numpy as np
 import math
@@ -8,12 +9,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MaxAbsScaler 
 from sklearn.model_selection  import train_test_split
 from sklearn.preprocessing import minmax_scale
+from keras.utils import plot_model
+from keras.models import load_model
 
-"""
+import matplotlib.pyplot as plt
+
 import keras
 from keras.layers import Dense
 from keras.models import Sequential
-"""
+from keras.callbacks import History
 import os
 from os import system, name 
 from threading import Thread
@@ -28,12 +32,12 @@ DATA_Y_FILENAME = OUTPUT_DIR + 'data_y'
 NUMPY_EXT = '.npy'
 
 CAMERA_WIDTH = 320
-CAMERA_HEIGHT = 240
+CAMERA_HEIGHT = 320
 
 NUM_THREADS = 4
-NUM_BATCH = 16
+NUM_BATCH = 256
 
-DATASET_FOLDER = 'driving_dataset/driving_dataset/'
+DATASET_FOLDER = './'
 DATASET_FILENAME = DATASET_FOLDER  + 'data.txt'
 MODEL_FILENAME = 'model.h5'
 
@@ -70,11 +74,16 @@ class ImportThread (Thread):
             angle = float(row[1])    
             
             # open image in b&w
-            frame = cv2.imread(DATASET_FOLDER+img, cv2.IMREAD_GRAYSCALE)
+            frame = cv2.imread(DATASET_FOLDER+img)
 
             # resize frame
             frame = cv2.resize(frame,(CAMERA_WIDTH,CAMERA_HEIGHT))
+
+            # apply threshold
+            ret, frame = cv2.threshold(frame, 50,255, cv2.THRESH_BINARY)
             
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
             # unroll
             frame = np.reshape(frame, (1,CAMERA_WIDTH*CAMERA_HEIGHT))
             
@@ -92,16 +101,22 @@ class ImportThread (Thread):
         np.save(DATA_Y_FILENAME+"_"+str(self.id), np.around(np.array(self.ylist), decimals=4))
 
 def BatchGenerator(batch_x, batch_y):
-    i=0
+    loopiter=0
     batch_len = len(batch_x)
+    #print("generator started with len " + str(batch_len))
     while True:
-        img = np.load(batch_x[i])
+        #print("generator " + str(loopiter))
+        img = np.load(batch_x[loopiter])
         img = img/255.0
-        labels = np.load(batch_y[i])
-        i = i + 1
-        if i >= batch_len:
-            i=0
-        yield (img, labels)
+        labels = np.load(batch_y[loopiter])
+        y = np.zeros((labels.shape[0], ANGLE_STEPS))
+        for i,angle in enumerate(labels):
+            idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
+            y[i,math.ceil(idx)] = 1
+        loopiter = loopiter + 1
+        if loopiter >= batch_len:
+            loopiter=0
+        yield (img, y)
 
 def main():
     # Import data
@@ -116,6 +131,7 @@ def main():
         with open(DATASET_FILENAME) as f:
             linelist = f.readlines()
 
+        #linelist = linelist[1:500]
         listlen = len(linelist)
         
         print("Dataset has " + str(listlen)+ " samples")
@@ -129,6 +145,8 @@ def main():
             y.append(line.split()[1])
         
         y = np.array(y)
+        #StdScaler = StandardScaler()
+        #MaxScaler = MaxAbsScaler()
         y_norm = MaxAbsScaler().fit_transform((StandardScaler().fit_transform(y.reshape(-1, 1))))        
 
         y = y_norm.tolist()
@@ -183,40 +201,101 @@ def main():
         print("> Some data elements are missing. exiting...")
         exit()
 
-    #for i,angle in enumerate(y_norm):
-    #    idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
-    #    y[i,math.ceil(idx)] = 1
-
     x_train, x_test, y_train, y_test = train_test_split(x_batch_list, y_batch_list, test_size = .1)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = .1)
 
-    # build simple NN
-    input_units = CAMERA_WIDTH*CAMERA_HEIGHT
-    inner_units = int(input_units/10)
-    output_units = ANGLE_STEPS # angle ranges from -1 to 1 in steps of 0.1
-
     bg_train = BatchGenerator(x_train, y_train)
-    bg_val = BatchGenerator(x_val, y_val)
+    bg_val = BatchGenerator(x_val, y_val)    
+    bg_test = BatchGenerator(x_test, y_test)
+
+    if (os.path.isfile(MODEL_FILENAME)):
+        print("> Loading existing NN model...")
+        model = load_model(MODEL_FILENAME)
+        y_test_vec = np.load(y_test[0])
+        x_test_vec = np.load(x_test[0])
+
+        predictions = model.predict(x_test_vec)
+
+        print ("predictions sample")
+        print(predictions[1:10])
+
+        print ("y sample")
+        y = np.zeros((y_test_vec.shape[0], ANGLE_STEPS))
+        for i,angle in enumerate(y_test_vec):
+            idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
+            y[i,math.ceil(idx)] = 1
+        print(y[1:10])
+
+
+    else:
+        print("Generating new NN model...")
+        # build simple NN
+        input_units = CAMERA_WIDTH*CAMERA_HEIGHT
+        output_units = ANGLE_STEPS # angle ranges from -1 to 1 in steps of 0.1
+
+
+        print("> Build Network")
+        model = Sequential()
+        model.add(Dense(units=int(input_units/100), activation='relu', input_dim=input_units))
+        model.add(Dense(units=int(input_units/100), activation='relu'))
+        model.add(Dense(units=int(512), activation='relu'))
+        model.add(Dense(units=int(125), activation='relu'))
+        #model.add(Dense(units=output_units, activation='softmax'))
+        model.add(Dense(units=1, activation='linear'))
+        print("> Compile and fit the Network")
+        hist = History()
+        model.compile(loss=keras.losses.mean_squared_error, optimizer='SGD', metrics=['accuracy'])
+        plot_model(model, to_file='model.png', show_shapes=True)
+
+        hist = model.fit_generator( bg_train, steps_per_epoch=NUM_BATCH,
+            validation_data=bg_val,
+            validation_steps=NUM_BATCH,
+            epochs=20)        
+        """
+        x = np.load(x_train[0])
+        y_test_vec = np.load(y_train[0])
+
+        y = np.zeros((y_test_vec.shape[0], ANGLE_STEPS))
+        for i,angle in enumerate(y_test_vec):
+            idx = (ANGLE_STEPS-1)/2*(angle)+(ANGLE_STEPS-1)/2
+            y[i,math.ceil(idx)] = 1
+
+        model.fit(x,y, batch_size= 1, epochs =10)
+        """
+
+        model.save(MODEL_FILENAME)
+        
+        loss_and_metrics = model.evaluate_generator(bg_test, steps=NUM_BATCH)
     
-    """
-    print("> Build Network")
-    model = Sequential()
-    model.add(Dense(units=inner_units, activation='relu', input_dim=input_units))
-    model.add(Dense(units=inner_units, activation='relu'))
-    model.add(Dense(units=output_units, activation='softmax'))
 
-    print("> Compile and fit the Network")
-    model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
-    model.fit_generator( bg_train, steps_per_epoch=math.ceil(len(x_train) / BATCH_SIZE),
-        validation_data=bg_val,
-	    validation_steps=math.ceil(len(x_train) / BATCH_SIZE),
-	    epochs=5)        
 
-    model.save(MODEL_FILENAME)
 
-    loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-    print(loss_and_metrics)
-    """
+        # Plot training & validation accuracy values
+        plt.plot(hist.history['acc'])
+        plt.plot(hist.history['val_acc'])
+        plt.title('Model accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.savefig('Model_accuracy.png')
+
+        # Plot training & validation loss values
+        plt.figure()
+        plt.plot(hist.history['loss'])
+        plt.plot(hist.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.savefig('Model_loss.png')
+        plt.show()
+
+        print("Fit metrics")
+        print(hist.history.keys())
+        print(hist.history.values())
+        print("Evaluate metrics")
+        print(loss_and_metrics)
+    
 
 if __name__ == "__main__":
     main()
