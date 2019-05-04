634a89a6dba4cb2be15d28ad5cd01d69e2a6da21
Francesco Viggiano <f.viggiano@ema-ic.it>
2019-05-05 00:53:32 +0200

    add RLNN class

--- reinforcement.py
+++ reinforcement.py
@@ -1,4 +1,8 @@
 import numpy as np
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.utils import to_categorical
 
 class RLQTable:
     def __init__(self, num_states, num_actions, assisted_decay = 0, eps = 0.2):
@@ -40,6 +44,68 @@ class RLQTable:
 
     def Learn(self, new_s, r):
         self.q_table[self.s, self.a] += r + self.lr * (self.lambd * np.max(self.q_table[new_s, :]) - self.q_table[self.s, self.a])
+
+    def GetTable(self):
+        print("Number of steps: ")
+        print("Assisted {:.1f}%, Random {:.1f}%, Learned {:.1f}%".format(self.iguid/self.steps*100, self.irand/self.steps*100, self.ilearn/self.steps*100))
+        return self.q_table
+
+class RLNN:
+    def __init__(self, num_states, num_actions, assisted_decay = 0, eps = 0.2):
+        self.lambd = 0.95
+        self.c = 3
+        self.eps = eps
+        self.eps_decay = 0.999
+        self.lr = 0.8
+        self.a = 0
+        self.s = 0
+        self.assisted_decay = assisted_decay
+        self.q_table = np.zeros((num_states, num_actions))
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.irand  = 0
+        self.iguid  = 0
+        self.ilearn = 0
+        self.steps  = 0.0
+        self.epochs = 50
+
+        self.model = Sequential()
+        self.model.add(Dense(self.num_states, activation='relu', input_dim=self.num_states))
+        self.model.add(Dense(self.num_actions, activation='linear'))
+        self.model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+
+    def Act(self, s, assisted_function):
+        # get new action
+        self.c *= self.assisted_decay
+
+        if self.c > 1:
+            self.a = assisted_function(s)
+            self.iguid +=1
+        elif self.c > 1 or np.random.random() < self.eps:
+            self.a = np.random.randint(0, self.num_actions)
+            self.irand +=1
+        else:
+            x = to_categorical(s, self.num_states).reshape((1,self.num_states))
+            p = self.model.predict(x)
+            self.a = np.argmax(p)
+            self.ilearn +=1
+
+        self.eps *= self.eps_decay
+        self.s = s
+        self.steps += 1
+        return self.a
+
+    def Learn(self, new_s, r):
+        new_x = to_categorical(new_s, self.num_states).reshape((1,self.num_states))
+        target = r + self.lambd * np.max(self.model.predict(new_x))
+        x = to_categorical(self.s, self.num_states).reshape((1,self.num_states))
+        target_vec = self.model.predict(x)[0]
+        target_vec[self.a] = target
+        self.model.fit(x, target_vec.reshape(-1, self.num_actions), epochs=int(self.epochs), verbose=0)
+        self.q_table[self.s,self.a] += r
+        if self.epochs > 10:
+            self.epochs *= self.assisted_decay
 
     def GetTable(self):
         print("Number of steps: ")
--- test-reinforcement.py
+++ test-reinforcement.py
@@ -16,27 +16,27 @@ def af(s):
     return -s+4
 
 def main():
-    num_states = 5
+    num_states = 8
     num_actions = 5
-    correction = np.linspace(-1,1,num_actions)
+    correction = [-2, -1, 0, 1, 2]#np.linspace(-1,1,num_actions)
 
-    rewards = [0, 0, 1, 0, 0] # TODO: Generalize this
+    rewards = [0, 2, 5, 2, 0] # TODO: Generalize this
     steps = 0
-    num_steps = 50
+    num_steps = 5
     for j in range(num_steps):
-        angle = 0.0
-        new_angle = 0.0
+        new_s = 0
         i = 0
         s = 0
         a = 0
         target_state = 2
         done = False
-        agent = rl.RLQTable(num_states,num_actions, 0.9, eps = 0.2)
+        #agent = rl.RLQTable(num_states,num_actions, 0.9, eps = 0.2)
+        agent = rl.RLNN(num_states,num_actions, 0.92, eps = 0.2)
         print("Run {}/{}".format(j,num_steps))
 
         while(done==False):
             # get reward
-            new_s = np.argmax(encodeLabels(new_angle, 5))
+            #new_s = new_angle
 
             #r = rewards[new_s]
 
@@ -46,30 +46,32 @@ def main():
             #s = new_s
             # predict new angle
             # angle = model.predict(frame)
-            r = rewards[new_s]
+            #r = rewards[new_s]
+            r = (new_s == target_state)*1
             agent.Learn(new_s, r)
             #print("State: {}, angle: {:.2}, correction: {}, new_angle: {:.1}, reward: {}, i: {}".format(new_s, angle, correction[a], new_angle, r, i))
             
-            angle = random.uniform(-1,1)
-            s = np.argmax(encodeLabels(angle, 5))
-
+            #angle = random.uniform(-1,1)
+            #s = np.argmax(encodeLabels(angle, 5))
+            s = random.randint(0,4)
             a = agent.Act(s, af)
 
             # get corrective angle based on the action
-            new_angle = angle + correction[a]
-            if (i == 30):
+            new_s = s + correction[a]
+            if (i == 15):
                 done = True
-            if (new_s == target_state):
+            if (new_s >= target_state-1 and new_s <= target_state+1):
                 i += 1
             else:
                 i = 0
             steps += 1
+            #print("new_s {}, i {} ".format(new_s, i))
 
     q_table = agent.GetTable()
     print(q_table)
     idxs = np.argmax(q_table, axis=1)
     print(idxs)
-    print(np.sum(abs(idxs-[4,3,2,1, 0])))
+    #print(np.sum(abs(idxs-[4,3,2,1, 0])))
     print("Average steps {}".format(steps/num_steps))
 
 if "__main__" == __name__:
